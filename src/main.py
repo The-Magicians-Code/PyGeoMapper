1e8848e88e1407aa0664ff8c1d9c5b53d2d787e6
Tanel Treuberg <tanetre@gmail.com>
2024-05-05 00:00:58 +0300

    GIS session and geocoder implementation tests

--- src/main.py
+++ src/main.py
@@ -1,1 +1,21 @@
+from arcgis.geocoding import geocode
+from arcgis.gis import GIS
+from pathlib import Path
+import pandas as pd
+# import pipreqs.pipreqs as pr
 
+# pr.main()
+
+fname = Path("source.csv").__str__()
+with open(fname, "r") as file:
+    objects = [line.rstrip() for line in file.readlines()]
+
+print(objects)
+
+# Create an anonymous GIS session
+gis = GIS()
+# Geocode the objects
+coded = {object: geocode(object)[0] for object in objects}
+
+df = pd.DataFrame(coded)
+print(df)

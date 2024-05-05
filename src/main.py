f6355bb8520b06eef5f1d7876443f25767568733
Tanel Treuberg <tanetre@gmail.com>
2024-05-05 12:30:48 +0300

    Geocoding results

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from arcgis.geocoding import geocode
 from arcgis.gis import GIS
 from pathlib import Path
-import pandas as pd
+# import pandas as pd
 # import pipreqs.pipreqs as pr
+import json
 
 # pr.main()
 
@@ -10,12 +11,11 @@ fname = Path("source.csv").__str__()
 with open(fname, "r") as file:
     objects = [line.rstrip() for line in file.readlines()]
 
-print(objects)
-
 # Create an anonymous GIS session
 gis = GIS()
 # Geocode the objects
-coded = {object: geocode(object)[0] for object in objects}
+coded = [geocode(object)[0] for object in objects]
+with open("res.json", "w") as file:
+    json.dump(coded, file, indent=4)
 
-df = pd.DataFrame(coded)
-print(df)
+# print(coded)

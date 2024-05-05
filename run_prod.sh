103a8b56b594f290d8ea80812f998a927290ce89
Tanel Treuberg <tanetre@gmail.com>
2024-05-06 01:43:02 +0300

    Bash automations added

--- run_prod.sh
+++ run_prod.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # @Author: Tanel Treuberg
 # @Github: https://github.com/The-Magicians-Code
-# @Description: Build and run Python dev development container
+# @Description: Build and run Python production development container
 
 tag="pygeomapper"
 name="geomap"
 
 docker build -t "$tag" . -f Dockerfile
-docker run --rm -it -d -v $(pwd):/src -p 80:8061 --name "$name" "$tag"
+docker run --rm -it -d -p 80:8061 --name "$name" "$tag"

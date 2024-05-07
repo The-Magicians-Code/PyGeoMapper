7f10363b3da252b0d06f82deb707dc4e7237c66b
Tanel Treuberg <tanetre@gmail.com>
2024-05-07 13:29:04 +0300

    Improved the prod deploy script

--- run_prod.sh
+++ run_prod.sh
@@ -3,8 +3,8 @@
 # @Github: https://github.com/The-Magicians-Code
 # @Description: Build and run Python production development container
 
-tag="pygeomapper"
-name="geomap"
+tag="pygeomapper_prod"
+name="geomap_prod"
 
-docker build -t "$tag" . -f Dockerfile
+docker build -t "$tag" . -f Dockerfile.prod
 docker run --rm -it -d -p 80:8061 --name "$name" "$tag"
